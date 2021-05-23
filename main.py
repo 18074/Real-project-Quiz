2811563e09e04a495ab204404ebef73ee69f491b
Vidhi Patel <>
2021-05-23 21:43:33 +0000

    I added exit page on home screen

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from tkinter import *
-
+from PIL import Image, ImageTk
 import random
 
+# The dictionary for my questions and answers
 global questions_answers
 names = []
 asked = []
@@ -21,7 +22,7 @@ questions_answers={
     
     }
 
- 
+ # The function randomiser which gives a random question each time.
 def randomiser():
     global qnum # The number given to the question from the dictionary above.
     qnum = random.randint(1,10) # Order of the questions is randomised
@@ -39,7 +40,19 @@ class Main:
         self.main_frame=Frame(parent, bg = background_color, padx=100, pady=100)
         #padx, pady how many pixels to pad widget, horizontally (x) and vertically (y), outside widget's borders.
         self.main_frame.grid() #This geometry manager organizes widgets in a table-like structure in the parent widget.
-               
+
+        # The code to add image in my home page
+        self.bg_image= Image.open("21.png")
+        self.bg_image= self.bg_image.resize((300,250), Image.ANTIALIAS )
+        self.bg_image= ImageTk.PhotoImage(self.bg_image)
+
+        self.main_frame=Frame(parent, bg= background_color)
+        self.main_frame.grid()
+
+        self.image_label= Label(self.main_frame, image=self.bg_image)
+        self.image_label.place(x=100, y=100, relheight=1)
+
+      
         # The main name label heading widget 
         self.caption_label=Label(self.main_frame, text="MATHS TEACHER CODE QUIZ", font=("Tw Cen MT","50","bold"),bg="#ce0018",)
         self.caption_label.grid(row=0, padx=20) 
@@ -66,47 +79,50 @@ class Main:
 
         
         #The exit button
-        self.quit_button = Button(self.main_frame, text="Exit", font=("Helvetica", "15", "bold"), bg="Red", command=self.endScreen)
-        self.quit_button.grid(row=8, sticky=E,padx=35, pady=35)
+        self.quit_button = Button(self.main_frame, text="Exit", font=("Helvetica", "15", "bold"), bg="Red", command=self.firstScreen)
+        self.quit_button.grid(row=8)
 
-       
-
-# the coding for the exit button on the home page and will lead to a pop screen saying Please try again next time giving users an option of wanting to quit before the quiz even starts.
-    def endScreen(self):
-        root.withdraw()
-        open_endscrn=End()
-
-class End:
-    def __init__(self):
-        background="Oldlace"
-        self.end.box= Toplevel(root)
-        self.end_box.title("End Box")
-
-        self.end_frame = Frame (self.end.box, width=1000, height=1000, bg=background)
-        self.end_frame.grid()
-
-        end_heading = Label (self.end_frame, text='Please try again next time', font=('Tw Cen MT',22, 'bold'), bg=background, pady=15)
-        end_hearing.grid(row=0)
-
-        exit_button= Button(self.end_frame, text='Exit', width=10, bg="IndianRed1", font=('Tw Cen MT',12,'bold'), command=self.close_end)
-        exit_button.grid(row=4, pady=20)
-
-        
-    def close_end(self):
-        self.end_box.destory()
-        root.withdraw()
-                
-
-                       
             
-
-    
     def name_collection(self):
         name=self.entry_box.get()
         names.append(name)
         #self.quiz_frame.destory()
         self.main_frame.grid_forget()
         Quiz(root)
+
+
+       
+
+# the coding for the exit button on the home page and will lead to a pop screen saying Please try again next time giving users an option of wanting to quit before the quiz even starts.
+
+
+    def firstScreen(self):
+        root.withdraw()
+        open_firstscrn=First()
+
+class First:
+    def __init__(self):
+        background="Oldlace"
+        self.first_box= Toplevel(root)
+        self.first_box.title("End Box")
+
+        self.first_frame = Frame (self.first_box, width=1000, height=1000, bg=background)
+        self.first_frame.grid()
+
+        first_heading = Label (self.first_frame, text='Thank You', font=('Tw Cen MT',22, 'bold'), bg=background, pady=15)
+        first_heading.grid(row=0)
+
+        exit_button= Button(self.first_frame, text='Exit', width=10, bg="IndianRed1", font=('Tw Cen MT',12,'bold'), command=self.close_first)
+        exit_button.grid(row=4, pady=20)
+
+        
+    def close_first(self):
+        self.first_box.destory()
+        root.withdraw()
+
+
+
+                      
       
        
 class Quiz:
@@ -125,30 +141,30 @@ class Quiz:
         #radio button 1
         self.rb1= Radiobutton(self.quiz_frame, text=questions_answers[qnum][1],font=("Helvetica","12"), bg=background_color,value=1,padx=10,pady=10,
                   variable=self.var1, indicator = 0, background = "light blue")
-        self.rb1.grid(row=2,sticky=W)
+        self.rb1.grid(row=2,sticky=W, padx=10, pady=10)
   
         #radio button 2
         self.rb2= Radiobutton(self.quiz_frame, text=questions_answers[qnum][2],font=("Helvetica","12"), bg=background_color,value=2,padx=10,pady=10,
                   variable=self.var1, indicator = 0, background = "light blue")
-        self.rb2.grid(row=3,sticky=W)
+        self.rb2.grid(row=3,sticky=W, padx=10, pady=10)
       
         #radio button 3
         self.rb3= Radiobutton(self.quiz_frame, text=questions_answers[qnum][3],font=("Helvetica","12"), bg=background_color,value=3,padx=10,pady=10,
                   variable=self.var1, indicator = 0, background = "light blue")
-        self.rb3.grid(row=4,sticky=W)
+        self.rb3.grid(row=4,sticky=W, padx=10, pady=10)
   
         #radio button 4
         self.rb4= Radiobutton(self.quiz_frame, text=questions_answers[qnum][4],font=("Helvetica","12"), bg=background_color,value=4,padx=10,pady=10,
                   variable=self.var1, indicator = 0, background = "light blue")
-        self.rb4.grid(row=5,sticky=W)
+        self.rb4.grid(row=5,sticky=W, padx=10, pady=10)
   
         #Confrim Button
         self.quizery_instance= Button(self.quiz_frame, text="Confirm", font=("Helevetica","13","bold"),bg="Gold", command=self.test_progress)
-        self.quizery_instance.grid(row=7, padx=5, pady=5)
+        self.quizery_instance.grid(row=4, padx=5, pady=5)
   
         #score label
         self.score_label=Label(self.quiz_frame,text="SCORE", font=("Tw Cen Mt","16"),bg=background_color,)
-        self.score_label.grid(row=8,padx=10,pady=1)
+        self.score_label.grid(row=6,padx=10,pady=1)
 
 
 
@@ -187,40 +203,54 @@ class Quiz:
                 if choice==questions_answers[qnum][5]: # if their choice is right
                     score+=1 
                     scr_label.configure(text=score)
-                    self.quizery_instance(text="Confirm")
+                    self.quizery_instance.config(text="Confirm")
                     self.questions_setup() # run this method to move to next question
                 else: #if the choice was wrong
                     score+=0
-                    scr_label.configure(text="The correct answer was:"+ questions_answers[qnum][5])
                     self.quizery_instance.configure(text="Confirm")
-                    self.questions.setup()
+                    self.questions_setup()
 
 
 # The exit button that will appear on the question page which will lead to a pop screen which will say Well Done and soon wil have a scoreboard.
     
-def endScreen(self):
-    root.withdraw()
-    open_endscrn=End()
+#The exit button
+        self.quit1_button = Button(self.quiz_frame, text="Exit", font=("Helvetica", "15", "bold"), bg="Red", command=self.lastScreen)
+        self.quit1_button.grid(row=8)
 
-class End:
+
+    def name_collection(self):
+        name=self.entry_box.get()
+        names.append(name)
+        #self.quiz_frame.destory()
+        self.quiz_frame.grid_forget()
+        Quiz(root)
+
+
+    def lastScreen(self):
+        root.withdraw()
+        open_lastscrn=Last()
+
+class Last:
     def __init__(self):
         background="Oldlace"
-        self.end.box= Toplevel(root)
-        self.end_box.title("End Box")
+        self.last_box= Toplevel(root)
+        self.last_box.title("End Box")
 
-        self.end_frame = Frame (self.end.box, width=1000, height=1000, bg=background)
-        self.end_frame.grid()
+        self.last_frame = Frame (self.last_box, width=1000, height=1000, bg=background)
+        self.last_frame.grid()
 
-        end_heading = Label (self.end_frame, text='Well Done', font=('Tw Cen MT',22, 'bold'), bg=background, pady=15)
-        end_hearing.grid(row=0)
+        last_heading = Label (self.last_frame, text='Thank You', font=('Tw Cen MT',22, 'bold'), bg=background, pady=15)
+        last_heading.grid(row=0)
 
-        exit_button= Button(self.end_frame, text='Exit', width=10, bg="IndianRed1", font=('Tw Cen MT',12,'bold'), command=self.close_end)
+        exit_button= Button(self.last_frame, text='Exit', width=10, bg="IndianRed1", font=('Tw Cen MT',12,'bold'), command=self.close_last)
         exit_button.grid(row=4, pady=20)
 
         
-def close_end(self):
-    self.end_box.destory()
-    root.withdraw()
+    def close_last(self):
+        self.last_box.destory()
+        root.withdraw()
+
+
 
 
     
